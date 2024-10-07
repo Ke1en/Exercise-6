dcf2585c08739c5584ab053372ec47bc2cad0750
Ke1en <Andpey-dok@mail.ru>
2024-10-07 22:18:31 +0600

    Added class calculator with overloaded sumInteger method

---
+++ src/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    public int sumIntegers(int a, int b) {
+        return a + b;
+    }
+
+    public int sumIntegers(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public int sumIntegers(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("New project!");
+        System.out.println(sumIntegers(5, 3));
+        System.out.println(sumIntegers(5, 3, 2));
+        Calculator calculator = new Calculator();
+        calculator.sumIntegers(5, 3);
+        calculator.sumIntegers(5, 3, 2);
+
+    }
+
+    public static int sumIntegers(int a, int b) {
+        return a + b;
+    }
+
+    public static int sumIntegers(int a, int b, int c) {
+        return a + b + c;
     }
 }
