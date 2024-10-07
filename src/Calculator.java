0762121abb79021d24aa1943189154386db97998
Ke1en <Andpey-dok@mail.ru>
2024-10-07 23:01:09 +0600

    Calculator class updated

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,4 +10,18 @@ public class Calculator {
     public int sumIntegers(int a, int b, int c, int d) {
         return a + b + c + d;
     }
+
+    public void printData(String name, int age) {
+        System.out.println("Name: " + name + " Age: " + age);
+    }
+
+    public void printData(int age, String name) {
+        System.out.println("Name: " + name + " Age: " + age);
+    }
+
+    public int calculateFactorial(int num) {
+        if (num == 1)
+            return 1;
+        return num * calculateFactorial(num - 1);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(sumIntegers(5, 3));
         System.out.println(sumIntegers(5, 3, 2));
+
         Calculator calculator = new Calculator();
+
         calculator.sumIntegers(5, 3);
         calculator.sumIntegers(5, 3, 2);
 
+        calculator.printData(25, "Dmitry");
+        calculator.printData("Dmitry", 25);
+
+        System.out.println(calculator.calculateFactorial(3));
     }
 
     public static int sumIntegers(int a, int b) {
